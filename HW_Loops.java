d37bfdc8c44408bf0a06f9f6daf8ebcc67127ae9
peterbolyrev <peterboldyrev@users.noreply.github.com>
2021-01-27 19:21:19 -0800

    adding task 3

--- HW_Loops.java
+++ HW_Loops.java
@@ -1,3 +1,5 @@
+//Peter Boldyrev // CSS 142 // HW Loops
+
 import java.util.*;
 
 public class HW_Loops {
@@ -12,6 +14,33 @@ public class HW_Loops {
     public static void overall(){
         String userInput = firstAsk();
         sanityCheck(userInput);
+        System.out.println("here is userInput after passing to sanity " +
+         userInput.toUpperCase());
+
+         //call next function from here re checks of C and num 2 and 3
+
+            //exercise #1 - count number of C occurences. 
+            //I have done it via concat of strings just to not use counter second time 
+            //but it can also be solved via counter 
+            occurenceC(userInput);
+
+            //exercise #2 Determine the fraction of cytosine
+            //  and guanine nucleotides. For example, if half of the 
+            //  nucleotides in the sequence are either "C" or "G", the fraction should be 
+            //  0.5.  If a sequence was "CGAATTTT", the fraction should be 0.25 (25% are C or G)
+            fractionCG(userInput);
+
+            // exercise #3 A DNA strand is actually made up of pairs of bases — 
+            // in effect, two strands that are cross-linked together. 
+            // These two strands are complementary: if you know one,
+            //  you can always determine the other, 
+            // or complement, because each nucleotide only pairs up with one other. 
+            // In particular, "A" and "T" are complements, as are "C" and "G". 
+            // So, for example, the complement of the sequence "AAGGTCT" would be "TTCCAGA". 
+            // Compute the complement of the input sequence.
+            compliment(userInput);
+
+
         //end of overall
     }
 
@@ -51,7 +80,7 @@ public class HW_Loops {
         Scanner keyboard = new Scanner(System.in);
         if(validCheck(userInput)){
             System.out.println("Thank you for adding the following valid string: "+userInput.toUpperCase());
-            //call next function from here re checks of C and num 2 and 3
+ 
         }
         else{
             System.out.println("Seems that not all characters are G,A,T or C. Here is what you entered: "
@@ -61,13 +90,72 @@ public class HW_Loops {
                 System.out.println("Goodbye!");
             }
             else{
+                // System.out.println("I am else ");
               sanityCheck(userInput2);
+
             }
         }
         //end of sanity check
     }
 
+    public static int occurenceC(String userInput){
+        String cOccurence = "";
+        for(int i=0;i<userInput.toUpperCase().length();i++){
+            if(userInput.toUpperCase().charAt(i)=='C'){
+                cOccurence = cOccurence+"C";
+            }
+        }
+        int numberOfC = cOccurence.length();
+        System.out.println("#1 Total the number of Cs entered is "+
+         numberOfC+".");
+        return numberOfC;
+        //end of C occurence
+    }
+    
+    public static double fractionCG(String userInput){
+        double counter = 0;
+        for(int i=0;i<userInput.toUpperCase().length();i++){
+            if(userInput.toUpperCase().charAt(i)=='C' ||
+            userInput.toUpperCase().charAt(i)=='G' ){
+                counter++;
+            }
+        }
+        double cgFraction = (counter/userInput.toUpperCase().length());
+        System.out.print("#2 There were "+counter +" Cs and Gs our of "+userInput.toUpperCase().length()+" So fraction is ");
+        System.out.printf("%.2f",cgFraction);
+        System.out.print(". Here is the percentage that C and G take of total: ");
+        System.out.printf("%.2f",cgFraction*100);
+        System.out.print("%");
+        System.out.println();
+        return cgFraction;
+    }
 
-    
-    
+
+    public static String compliment(String userInput){
+        String compl = "";
+        for(int i=0;i<userInput.toUpperCase().length();i++){
+            if(userInput.toUpperCase().charAt(i)=='A'){
+                compl=compl+"T";
+            }
+            else if(userInput.toUpperCase().charAt(i)=='T'){
+                compl=compl+"A";
+            }
+            
+            else if(userInput.toUpperCase().charAt(i)=='C'){
+                compl=compl+"G";
+            }
+            else if(userInput.toUpperCase().charAt(i)=='G'){
+                compl=compl+"C";
+            }
+        }
+        System.out.println("here is compliment string "+compl);
+        return compl;
+
+        //end of compliment method
+    }
+
+
+
+
+    //end of class HW_Loops
 }
