80bee6c6233b16404cc5885d31694ed0defd16dc
Peter <53663056+peterboldyrev@users.noreply.github.com>
2021-02-12 21:40:24 -0800

    Add files via upload
    adding part 2 
    
    Part 2
    In a class called Diary (template is provided), write a program that prompts users to enter the date as three integers separated by spaces (i.e mm dd yyyy) then it prompts them enter as many lines of prose they wish (for their to-do’s list or diary entry). Your program should store their entries at the end of a file called “diaryLog.txt” and old data should NOT be erased. Each entry should be formatted as follows (example for a diary entry entered on 7/14/2017):

---
+++ Lab6.java
@@ -0,0 +1,60 @@
+import java.io.FileInputStream; //"turns" the file into a read stream
+import java.util.Scanner; //reads from stream
+import java.io.IOException;
+import java.io.File;
+import java.io.FileOutputStream; //"turns" the file into a write stream
+import java.io.PrintWriter; //writes to the stream
+
+import java.io.FileNotFoundException; //this exception must be caught and handled when dealing with streams!
+
+
+public class Lab6 {
+    public static void main(String[] args){
+
+        diaryLog();
+        // end of main
+    }
+
+//part 2 
+    public static void diaryLog(){
+
+
+
+        Scanner keyboard = new Scanner(System.in);
+    System.out.println("Please enter a date of the entry >");
+        String userDate = keyboard.nextLine();
+        System.out.println("Now tell the diary all the secrets >");
+        String userInput = keyboard.nextLine();
+
+        
+        PrintWriter outputStream = null; //declaring the outputstream
+
+        try {
+            outputStream = new PrintWriter(new FileOutputStream("diaryLog.txt",true));
+        } catch (FileNotFoundException e) {
+
+            System.out.println("Error creating/accessing the file stats.txt."); 
+            System.exit(0);
+        }
+
+        System.out.println("Writing to file");
+        outputStream.println(userDate);
+        outputStream.println(userInput);
+        outputStream.println();
+        outputStream.close();
+
+        System.out.println("Do you have more secrets? Type yes to add more or anything to exit >");
+        String userAction = keyboard.next();
+        
+        if(userAction.toLowerCase().equals("yes")){
+            diaryLog();
+        }
+        else{
+            System.out.println("Ok thank you!");
+        }
+
+
+
+    }
+    //end of class
+}
