3517e07f41a0c6e5a9df032aacd89f520a925686
Peter <53663056+peterboldyrev@users.noreply.github.com>
2021-02-12 21:14:13 -0800

    Add files via upload
    working version

--- HW_Loops_Files.java
+++ HW_Loops_Files.java
@@ -1,18 +1,43 @@
+//Peter Boldyrev CSS 142
+//Feb 12 - HW Loops_FIles (I/O)
+
 import java.util.Scanner;
+import java.util.NoSuchElementException;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-//http://www.cplusplus.com/forum/beginner/170504/
+
 public class HW_Loops_Files{
 
     public static void main(String[] args){
-        String nextLine;
+        String nextLine; 
         double programWeight=0;
         double midtermWeight=0;
         double finalExamweight=0;
+        int  gradeCounter=0; // counter that tracks number of grades (i.e 161, 263 etc)
+
+        //declaring scanner to check the number of grades in place
+        Scanner counterScanner = null;
+
+        try {
+            //adding data from txt file
+            counterScanner = new Scanner (new FileInputStream("courseData.txt"));
+        } 
+        //catching error if any to proceed further 
+        catch (FileNotFoundException e) {
+            System.out.println("Here is a filenotfound exception");
+            System.exit(0);
+            
+        }
+        
+        while(counterScanner.hasNext()){//looping through scanner 
+            if(counterScanner.nextLine().equals("0")){ 
+                gradeCounter++; //counting number of grades (i.e. 161, 263 etc)
+            }
+        }
         
 
         Scanner inputStream = null;
-
+// adding another scanner to loop since previous is at the end + handling the errors via try - catch 
         try {
             inputStream = new Scanner(new FileInputStream("courseData.txt"));
         } 
@@ -21,10 +46,8 @@ public class HW_Loops_Files{
             System.exit(0);
             
         }
-
-        //getting exam weights thru line 35
+        //getting exam weights start 
         nextLine = inputStream.nextLine();
-        
 
         Scanner weight = new Scanner(nextLine);
         while(weight.hasNext()){
@@ -33,38 +56,94 @@ public class HW_Loops_Files{
             finalExamweight = weight.nextDouble();
             
         }
-       
+        //getting exam weights end
 
-        String groupNum = inputStream.nextLine();
-        double classAverage = 0;
-        int counter = 0;
+        //starts grading method for every grade identified via gradeCounter
+        //passing Scanner and weights from lines 46-56
+        for(int i =0;i<gradeCounter;i++){
+            gradingMethod(inputStream, programWeight, midtermWeight, finalExamweight );
+        }
+        
+        // end of main
+    }
+
+    //grading method 
+
+    public static void gradingMethod(Scanner inputStream, 
+    double programWeight, 
+    double midtermWeight, 
+    double finalExamweight){
+
+//getting the group number based on the file structure
+    String groupNum = inputStream.nextLine(); 
+
+    double classAverage = 0;
+    int studentsPerGradeCounter = 0; // counting students per grade to get weighted average
+    double weightedAverage = 0; // declaring weightedAverage as zero
+    String passFail =""; //declaring a String for pass/fail value
+
+    //retrieve separated values for studentID programScore midterm score and finalScore
+    int studentID = 0;
+    int programScore = 0;
+    int midtermScore = 0;
+    int finalScore = 0;
+
+        System.out.println("\nGrade data for class "+groupNum+"\n ");
+        System.out.println(" ID  PROG  MID  FINAL  WEIGHTED  GRADE ");
+        System.out.println(" --  ----  ---  -----  -------  ------"); 
+        //looping through the scanner
         while(inputStream.hasNext()){
             
             String line = inputStream.nextLine();
+            
+            try {
+                //creating another scanner to get values of studentIT programScore midtermScore and finalScore
+                Scanner linScanner2 = new Scanner (line);
+            while(linScanner2.hasNext()){
+                
+                studentID = linScanner2.nextInt();
+                programScore = linScanner2.nextInt();
+                midtermScore = linScanner2.nextInt();
+                finalScore = linScanner2.nextInt();
+                
+            }
+            } catch (NoSuchElementException e) {
+                System.out.print("");
+            }
+            
             if(line.equals("0")){
                 break;
+                //this will start the next iteration of grading method
             }
-            classAverage+=studentAverage(line, programWeight,midtermWeight,finalExamweight);
-            System.out.println(studentAverage(line, programWeight,midtermWeight,finalExamweight));
-            System.out.println(classAverage + " this is class average");
-            counter++;
+
+            
+            
+            weightedAverage = weightedAverage(line, programWeight,midtermWeight,finalExamweight);
+            classAverage+=weightedAverage;
+            if (weightedAverage>=70){
+                passFail="Pass";
+            }
+            else{
+                passFail="Fail";
+            }
+            
+           System.out.print(studentID +"  " +programScore + "    " +midtermScore + "   " +finalScore + "     ");
+           System.out.printf("%.2f", weightedAverage);
+           System.out.print("   " + passFail);
+           System.out.println();
+            studentsPerGradeCounter++;
            
            
         }
         
-        System.out.println("class average is "+ (classAverage/(double)counter));
-        
-        
+        System.out.println("Class average is "+ (classAverage/(double)studentsPerGradeCounter));
        
-        
-      
+    
+    //end of grading method   
+    };
 
-       
-        // end of main
-    }
-
-        public static double studentAverage(String line, double programWeight, double midtermWeight, double finalExamweight){
-            int studentId = 0;
+        public static double weightedAverage(String line, double programWeight, double midtermWeight, double finalExamweight){
+        int studentId = 0;
         int programScore = 0;
         int midtermScore = 0;
         int finalScore = 0;
@@ -72,17 +151,22 @@ public class HW_Loops_Files{
             // System.out.println(line);
             Scanner linScanner3 = new Scanner (line);
             while(linScanner3.hasNext()){
+                
                 studentId = linScanner3.nextInt();
                 programScore = linScanner3.nextInt();
                 midtermScore = linScanner3.nextInt();
                 finalScore = linScanner3.nextInt();
             }
-            weightedAverage = ((double)programScore*programWeight)+((double)midtermScore*midtermWeight)+((double)finalExamweight*finalScore);
+            weightedAverage = ((double)programScore*programWeight)+
+            ((double)midtermScore*midtermWeight)+
+            ((double)finalExamweight*finalScore);
             //   System.out.println(weightedAverage);
 
             //end of studentAverage
             return weightedAverage;
         }
+
+       
             
     //end of class
 }
